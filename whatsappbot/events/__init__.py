ed6021ccfcbaa109775368908e813de4ce304363
Elielson68 <elielsonbr.com@gmail.com>
2022-05-18 01:59:09 -0300

    Fix: funções não estavam sendo chmadas pelo bot

--- whatsappbot/events/__init__.py
+++ whatsappbot/events/__init__.py
@@ -1,6 +1,7 @@
 from chatbot import Chat
-from .verify import * 
+from .agendai_verify import *
 from .register import *
 from .alert import *
+
 
 marcia = Chat(default_template="whatsappbot/templates/conversation.j2", language='pt-br')
