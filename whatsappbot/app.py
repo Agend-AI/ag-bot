12f65ff48d8be71789462f5118b2e8102d69b0c2
Elielson68 <elielsonbr.com@gmail.com>
2022-05-18 21:13:59 -0300

    feat: thread para utilizar as funcoes de datas

--- whatsappbot/app.py
+++ whatsappbot/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from .extensions.configuration import load_modules
 from .extensions.configuration import init_app as init_conf
+from .services.weekday import start_thread
+
 
 def create_app():
     app = Flask(__name__)
+    start_thread()
     init_conf(app)
     
     load_modules(app)
