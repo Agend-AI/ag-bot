88e15342dbf5177916b6b7702f84169fa794f421
Elielson68 <elielsonbr.com@gmail.com>
2022-05-23 20:33:02 -0300

    revertendo alterações modificando nome e pares do chatbot

--- whatsappbot/events/__init__.py
+++ whatsappbot/events/__init__.py
@@ -4,7 +4,7 @@ import os
 users_conversation = {
 
 }
-agendai = Chat(default_template=os.environ.get("TEMPLATE_PATH") or "../templates/conversation.j2", language='pt-br')
+agendai = Chat(pairs=os.environ.get("TEMPLATE_PATH") or "../templates/conversation.template", default_template=os.environ.get("TEMPLATE_PATH") or "../templates/conversation.template", language='pt-br')
 
 
 
